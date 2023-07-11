7867d3a00e364bec61a0ad6796668b5c4ab6d762
prajjwal-jain <101256689+prajjwal-jain@users.noreply.github.com>
2023-07-11 20:31:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname,"./client/build")))
 app.use("/api/v1/auth",authRoutes);
 app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes);
-const PORT=process.env.PORT||8080;
+const PORT=process.env.PORT;
 app.use("*",function(req,res){
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
 })
